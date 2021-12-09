45bbf5113034010edd5d3acf9dffb47b8829690e
elias <eliasgamezdelgadillo1999@gmail.com>
2021-12-09 02:54:48 -0600

    first commit

--- guardar/guardar_ansiedad.php
+++ guardar/guardar_ansiedad.php
@@ -137,7 +137,7 @@ $insertar = $mysqli->query("INSERT INTO ansiedad (ansiedad.Id_ansiedad, ansiedad
                                         ansiedad.Pregunta_40,ansiedad.Escala_estado, ansiedad.Escala_rasgo, 
                                         ansiedad.Estado, ansiedad.Rasgo, ansiedad.Nombre,ansiedad.Fecha) 
                                         VALUES(NULL,$id,$p1,$p2,$p3,$p4,$p5,$p6,$p7,$p8,
-                                        $p9,$p10,$p11,$p12,$p13,$p14,$p15,'$p16,$p17,$p18,
+                                        $p9,$p10,$p11,$p12,$p13,$p14,$p15,$p16,$p17,$p18,
                                         $p19,$p20,$p21,$p22,$p23,$p24,$p25,$p26,$p27,$p28,
                                         $p29,$p30,$p31,$p32,$p33,$p34,$p35,$p36,$p37,$p38,
                                         $p39,$p40,$ab,$aabb,'$estado','$rasg',NULL,'$date')");
